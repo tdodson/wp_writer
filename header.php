6b6dfa7dfec3b8998a1e2902845be4394ae003a3
tdodson <dodson.t@gmail.com>
2017-11-27 14:41:33 -0500

    removed primary menu button from header.php. Menu will respond, but no real need to hide it; user will scroll.

--- header.php
+++ header.php
@@ -30,7 +30,6 @@
 
 	<header id="masthead" class="site-header">
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscores_sass' ); ?></button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'primary',
