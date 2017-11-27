c598c53a4dafba60b5f529f028f7ea989403ac65
tdodson <dodson.t@gmail.com>
2017-11-27 12:20:29 -0500

    blank CSS slate for primary and secondary menus at _header-menu.scss

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 			?>
 		</nav><!-- #site-navigation -->
 
-		<nav id="social-navigation" class="secondary-navigation">
+		<nav id="social-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="social-menu" aria-expanded="false"><?php esc_html_e( 'Social Menu', 'underscores_sass' ); ?></button>
 			<?php
 				wp_nav_menu( array(
