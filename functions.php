12f1e6499a400245962e83a6cd9ab6778ccae0d8
tdodson <dodson.t@gmail.com>
2017-11-30 21:15:58 -0500

    story custom content type; some minor image styling.

--- functions.php
+++ functions.php
@@ -124,6 +124,21 @@ add_action( 'after_setup_theme', 'underscores_sass_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function underscores_sass_widgets_init() {
+	
+	function create_post_type() {
+  		register_post_type( 'u_sass_story',
+    		array(
+      			'labels' => array(
+        		'name' => __( 'Story' ),
+        		'singular_name' => __( 'Story' )
+      		),
+      		'public' => true,
+      		'has_archive' => false,
+    	)
+  	);
+}
+add_action( 'init', 'create_post_type' );
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'underscores_sass' ),
 		'id'            => 'sidebar-1',
@@ -151,6 +166,8 @@ function underscores_sass_scripts() {
 	wp_enqueue_script( 'underscores_sass-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'underscores_sass-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'underscores_sass-functions', get_template_directory_uri() . '/js/functions.js', array(), '20171130', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
