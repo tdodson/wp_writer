a2136553e50c83106fe367608abf692676719747
tdodson <dodson.t@gmail.com>
2017-12-14 21:06:31 -0500

    deleted excerpts.php and set index.php to point back to content.php. Added conditional is_singular() statement to content.php in combination with the_excerpt() and the_content() to make archive pages excerpts and single pages full content.

--- template-parts/content.php
+++ template-parts/content.php
@@ -41,24 +41,28 @@
 
 	<div class="entry-content">
 		<?php
-			/* the_excerpt(); */
+			/* Could use the !is_singular() function here with the_excerpt(); instead of creating excerpt.php? */
 
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'underscores_sass' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) ); 
-			 wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'underscores_sass' ),
-				'after'  => '</div>',
-			) ); 
+			if ( !is_singular() ) :
+				the_excerpt();
+			else :
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'underscores_sass' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) ); 
+				 wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'underscores_sass' ),
+					'after'  => '</div>',
+				) );
+			endif; 
 		?>
 	</div><!-- .entry-content -->
 
