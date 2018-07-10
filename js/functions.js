5f092eee0f2d09c586da787202c00a76bea88892
Thomas Dodson <tdodson@bu.edu>
2018-07-10 10:57:10 -0400

    text overlay now triggered by jquery mouse .on()

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,13 @@
 (function($) {
 	$('figure.wp-caption.aligncenter').removeAttr('style');
 	$('img.aligncenter').wrap('<figure class="centered-image" />');
+	$(document).ready(function() {
+		$('.stories > a').on({
+			mouseenter: function() {
+		    	$(this).find('figcaption').removeClass('text-off').addClass('text-on');
+			}, mouseleave: function() {
+				$(this).find('figcaption').removeClass('text-on').addClass('text-off');
+			}
+		});
+	});
 }) (jQuery);
