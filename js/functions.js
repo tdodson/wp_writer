db8f7655d876536f3e6ab5103695bf52734efb24
Thomas Dodson <tdodson@bu.edu>
2018-07-10 13:28:08 -0400

    pure css hover for stories grid

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,4 @@
 (function($) {
 	$('figure.wp-caption.aligncenter').removeAttr('style');
 	$('img.aligncenter').wrap('<figure class="centered-image" />');
-	$(document).ready(function() {
-		$('.stories > a').on({
-			mouseenter: function() {
-		    	$(this).find('figcaption').removeClass('text-off').addClass('text-on');
-			}, mouseleave: function() {
-				$(this).find('figcaption').removeClass('text-on').addClass('text-off');
-			}
-		});
-	});
 }) (jQuery);
